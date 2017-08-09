3dca1fef5c0af66fbe824ae93bb84ba5921dc42f
Paweł Talaga <Pawe Talaga>
2017-08-09 19:51:34 +0200

    first

--- hoot/tests.py
+++ hoot/tests.py
@@ -1,3 +1,76 @@
-from django.test import TestCase
+import unittest
+from django.core.urlresolvers import reverse
+from django.test import Client
+from .models import user
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
+from django.contrib.contenttypes.models import ContentType
 
-# Create your tests here.
+
+def create_django_contrib_auth_models_user(**kwargs):
+    defaults = {}
+    defaults["username"] = "username"
+    defaults["email"] = "username@tempurl.com"
+    defaults.update(**kwargs)
+    return User.objects.create(**defaults)
+
+
+def create_django_contrib_auth_models_group(**kwargs):
+    defaults = {}
+    defaults["name"] = "group"
+    defaults.update(**kwargs)
+    return Group.objects.create(**defaults)
+
+
+def create_django_contrib_contenttypes_models_contenttype(**kwargs):
+    defaults = {}
+    defaults.update(**kwargs)
+    return ContentType.objects.create(**defaults)
+
+
+def create_user(**kwargs):
+    defaults = {}
+    defaults["name"] = "name"
+    defaults["password"] = "password"
+    defaults.update(**kwargs)
+    return user.objects.create(**defaults)
+
+
+class userViewTest(unittest.TestCase):
+    '''
+    Tests for user
+    '''
+    def setUp(self):
+        self.client = Client()
+
+    def test_list_user(self):
+        url = reverse('hoot_user_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_user(self):
+        url = reverse('hoot_user_create')
+        data = {
+            "name": "name",
+            "password": "password",
+        }
+        response = self.client.post(url, data=data)
+        self.assertEqual(response.status_code, 302)
+
+    def test_detail_user(self):
+        user = create_user()
+        url = reverse('hoot_user_detail', args=[user.pk,])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_user(self):
+        user = create_user()
+        data = {
+            "name": "name",
+            "password": "password",
+        }
+        url = reverse('hoot_user_update', args=[user.pk,])
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)
+
+
--- hoot/views.py
+++ hoot/views.py
@@ -1,23 +1,22 @@
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.views.generic import DetailView, ListView, UpdateView, CreateView
+from .models import user
+from .forms import userForm
 
-# Create your views here.
-def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
 
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('Username')
-            raw_password = form.cleaned_data.get('Password')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'login.html', {'form': form})
+class userListView(ListView):
+    model = user
+
+
+class userCreateView(CreateView):
+    model = user
+    form_class = userForm
+
+
+class userDetailView(DetailView):
+    model = user
+
+
+class userUpdateView(UpdateView):
+    model = user
+    form_class = userForm
+
