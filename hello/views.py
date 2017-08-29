93ed31008000e8e4d8305f6f45d62de35952bdb6
Paweł Talaga <Pawe Talaga>
2017-08-29 22:53:14 +0200

    try on posting

--- hello/views.py
+++ hello/views.py
@@ -15,7 +15,7 @@ def map(request):
             y = request.POST['y']
             with open("/hello/static/rgdata.xht","a") as datas:
                 datas.write('<entry> <title>NOWY_POST</title> <published>DATA</published><content type="html">ZAWARTOSC HTML</content> <author> <name>AUTOR</name> </author> <georss:point>'+str(x)+' '+str(y)+'</georss:point> <geo:lat>'+str(x)+'</geo:lat> <geo:long>'+str(y)+'</geo:long> <woe:woeid>142344433</woe:woeid> </entry>')
-            return render(request, 'map.html') # if everything is OK
+            return HttpResponse(str(x)+' '+str(y)+' It works pirlitirli!') # if everything is OK
     # nothing went well
     return render(request, 'map.html')
 
