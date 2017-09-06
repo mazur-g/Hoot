470a899c790206fc6df66cfe4dd5f1f4c9dc7637
Paweł Talaga <wajdamuzyki@gmail.com>
2017-09-06 16:17:47 +0200

    Forms sort of try

--- hello/views.py
+++ hello/views.py
@@ -20,12 +20,13 @@ def map(request):
         if request.method == "POST":
             form = GeoMessageForm(request.POST)
             if form.is_valid():
-                return HttpResponse('It works pirlitirli!')
+                return HttpResponse(str(form.message)+' It works pirlitirli!')
                 #with open("/hello/static/rgdata.xht","a") as datas:
                 #datas.write('<entry> <title>NOWY_POST</title> <published>DATA</published><content type="html">ZAWARTOSC HTML</content> <author> <name>AUTOR</name> </author> <georss:point>'+str(x)+' '+str(y)+'</georss:point> <geo:lat>'+str(x)+'</geo:lat> <geo:long>'+str(y)+'</geo:long> <woe:woeid>142344433</woe:woeid> </entry>')
 
         # nothing went well
             return render(request, 'map.html', {'form': form})
+        return render(request, 'map.html', {'form': form})
     else:
         return redirect("{% url 'accounts:login' %}", permanent=False)
 
