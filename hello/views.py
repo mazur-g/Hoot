d3d44299de1817ac84f7a3bb5fa6b2795e668ac2
Paweł Talaga <wajdamuzykI@gmail.com>
2017-09-06 21:57:32 +0200

    Repairs

--- hello/views.py
+++ hello/views.py
@@ -26,7 +26,7 @@ def map(request):
                 ip = request.META.get('REMOTE_ADDR', None)
                 if ip:
                     location = g.coords(ip)
-                return HttpResponse("Twoja lokacja ciulu: "+str(location)+'\nTwoja wiadomość ciulu: '+str(geo_message))
+                return HttpResponse("Twoja lokacja ciulu: "+str(location[0])+', '+str(location[1])+'\nTwoja wiadomość ciulu: '+str(geo_message))
             #with open("/hello/static/rgdata.xht","a") as datas:
             #datas.write('<entry> <title>NOWY_POST</title> <published>DATA</published><content type="html">ZAWARTOSC HTML</content> <author> <name>AUTOR</name> </author> <georss:point>'+str(x)+' '+str(y)+'</georss:point> <geo:lat>'+str(x)+'</geo:lat> <geo:long>'+str(y)+'</geo:long> <woe:woeid>142344433</woe:woeid> </entry>')
         else:
